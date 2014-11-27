c77f2a1a69b17b184e04602046f8fa44400ed9e7
Francisco Baila <francisco_baila@hotmail.com>
2014-11-27 23:11:26 +0000

    preview book

---
+++ preview.php
@@ -0,0 +1,32 @@
+  <?php
+  include ('includes/header.php');
+  ?>
+
+  <div id="container">
+    <div id="book-preview">
+      <div class="pictogram">
+        <img class="img-insider" src="img/picto.png" />
+      </div>
+      <img class="img-book" src="img/book.jpg" />
+    </div>
+    <div id="interaction">
+      <div id="interaction-text">
+        “Bryant & May and the Bleeding Heart” is the latest case for Christopher Fowler’s detective from the Peculiar Crimes Unit.
+      </div>
+    </div>
+    <div id="links">
+      <div class="link">
+        View Comments
+      </div>
+      <div class="link">
+        Comment
+      </div>
+      <div class="link">
+        View Reviews
+      </div>
+    </div>
+  </div>
+
+  <?php
+  include ('includes/footer.php');
+  ?>
