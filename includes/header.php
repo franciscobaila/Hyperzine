7e6cbf3854994d739d85ac9f69d25a2212d33ce8
sergiomrebelo <sergiorebelo@me.com>
2014-11-27 21:44:05 +0000

    includes header

---
+++ includes/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta name="description" content="">
+    <meta name="author" content="Grupo 17">
+	<title>Hyperzine</title>
+	
+	<link rel="shortcut icon" href="../favicon.ico" type="image/x-icon">
+	<link rel="icon" href="../favicon.ico" type="image/x-icon">
+     <!-- CSS --> 
+     <link href="css/style.css" rel="stylesheet" type="text/css">
+     <link href="css/sergio-main.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<section id="h-user" class="header">
+	<nav id="nav-h"> 
+	
+		<div class="element-header">logo</div>
+		<div class="element-header">procurar</div>
+		<div class="element-header">definições</div>
+		<div class="element-header">estante</div>
+		<div class="element-header">biblioteca</div>
+		<div class="element-header">publico</div>
+	</nav>
+</section>
