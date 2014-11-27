22e6d1112b0ea85f3f0c39caee87ee5b247af315
Francisco Baila <francisco_baila@hotmail.com>
2014-11-27 22:31:32 +0000

    footer

--- library.php
+++ library.php
@@ -39,10 +39,6 @@ include ('includes/header.php');
       </div>
   </div>
 
-
-    <!-- Javascript at the bottom -->
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <!-- for testing -->
-    <script type="text/javascript" src="js/main.js"></script>
-</body>
-</html>
+<?php
+include ('includes/footer.php');
+?>
