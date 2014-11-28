17a10c470c4872b13199edcac9d11e454fcb825d
Francisco Baila <francisco_baila@hotmail.com>
2014-11-28 00:09:52 +0000

    create review page

---
+++ create-review.php
@@ -0,0 +1,19 @@
+
+  <?php
+  include ('includes/header.php');
+  ?>
+  <div id="container">
+      <div id="write-review">
+        <textarea class="review-textarea" placeholder="Write your review."></textarea>
+      </div>
+
+      <form action="" id="classification-form">
+        <input type="number" placeholder="0 to 10" id="input-review" min="0" max="10">
+        <input type="submit" value="Submit" id="input-review-submit">
+      </form>
+  </div>
+
+
+  <?php
+  include ('includes/footer.php');
+  ?>
