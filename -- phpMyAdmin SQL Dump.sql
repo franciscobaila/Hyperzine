da872e67c0affca4b471261752e86f1e5954f30b
xufuou <xufuou@gmail.com>
2014-12-01 01:18:15 +0000

    database v2

--- -- phpMyAdmin SQL Dump.sql
+++ -- phpMyAdmin SQL Dump.sql
@@ -2,8 +2,8 @@
 -- version 4.2.10
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Dec 01, 2014 at 12:59 AM
+-- Host: localhost:8889
+-- Generation Time: Dec 01, 2014 at 02:13 AM
 -- Server version: 5.5.38
 -- PHP Version: 5.6.2
 
@@ -13,28 +13,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `Hyperzine`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `books`
---
-
-CREATE TABLE `books` (
-`book_id` int(100) NOT NULL,
-  `book_name` varchar(100) NOT NULL,
-  `author` varchar(100) NOT NULL,
-  `category` varchar(100) NOT NULL,
-  `book_image` varchar(1000) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `books`
---
-
-INSERT INTO `books` (`book_id`, `book_name`, `author`, `category`, `book_image`) VALUES
-(1, 'The Design of Everyday Things', 'Donald A. Norman', 'Design', 'http://yangsu.github.io/images/teapot-for-masochists.jpeg'),
-(2, 'Model-Based Design and Evaluation of Interactive Applications', 'Paterno, Fabio', 'Design', 'https://images.springer.com/sgw/books/medium/9781852331559.jpg');
 
 -- --------------------------------------------------------
 
@@ -53,20 +31,14 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `username`, `password`) VALUES
-(1, 'sergiomrebelo', '1234'),
-(2, 'xufuou', '1234'),
-(3, 'franciscob', '1234'),
-(5, 'jessicaf', '1234');
+(1, 'sergiomrebelo', '12345678'),
+(2, 'xufuou', '12345678'),
+(3, 'franciscob', '12345678'),
+(5, 'jessicaf', '12345678');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `books`
---
-ALTER TABLE `books`
- ADD PRIMARY KEY (`book_id`);
 
 --
 -- Indexes for table `users`
@@ -78,11 +50,6 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for dumped tables
 --
 
---
--- AUTO_INCREMENT for table `books`
---
-ALTER TABLE `books`
-MODIFY `book_id` int(100) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `users`
 --
