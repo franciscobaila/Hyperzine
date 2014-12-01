6ad56b0e14fa91db9bc99aacb9bf1bcb1dd68e19
Francisco Baila <francisco_baila@hotmail.com>
2014-12-01 01:51:34 +0000

    header titles

--- includes/header.php
+++ includes/header.php
@@ -42,11 +42,11 @@
 				<img width="30px"src="http://iconmonstr.com/g/gd/makefg.php?i=s2/default/iconmonstr-gear-3-icon.png&r=189&g=195&b=199"/>
 
 			</div>
-			<div class="element-header" id="shelf-header">
+			<div class="element-header" id="shelf-header" title="shelf">
 				<!-- catalog -->
 				<img width="35px" src="http://iconmonstr.com/g/gd/makefg.php?i=s2/default/iconmonstr-book-17-icon.png&r=189&g=195&b=199">
 			</div>
-			<div class="element-header" id="library-header">
+			<div class="element-header" id="library-header" title="library">
 				<!-- library -->
 				<img width="30px"src="http://iconmonstr.com/g/gd/makefg.php?i=s2/default/iconmonstr-help-8-icon.png&r=189&g=195&b=199"/>
 			</div>
