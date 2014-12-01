d39b75a72a455aa95581f5d0297783fb5f7246ee
xufuou <xufuou@gmail.com>
2014-12-01 04:18:31 +0000

    search update

--- search.php
+++ search.php
@@ -1,11 +1,12 @@
 <?php
 include ("includes/connect.php");
 $data = mysql_escape_string($_POST['search']);
+echo $data;
+$query = "select * from books where book_name like '"%.$data.%"'or author like '"%.$data.%"'or category like '"%.$data.%"";
 
-
-$search = "select * from books where category like '".%$data%."'";
-$result = mysql_query($search);
+$result = mysql_query($query);
 $num = mysql_num_rows($result);
+
 if ($result) {
 	$num = mysql_num_rows($result);
 	if ($num >= 1) {
