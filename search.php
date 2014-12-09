b53cfdc8947082511832475917ac70e5174dbf4c
xufuou <xufuou@gmail.com>
2014-12-09 00:37:06 +0000

    search image

--- search.php
+++ search.php
@@ -2,42 +2,38 @@
 include ('includes/header.php');?>
 
  <div id="container">
-    <div id="search-message">Search results:</div>
-      <div id="books-container">
-        <!-- book example ----------- -->
-        <div id="book-1" class="book cenas">
-          <div class="pictogram">
-            <img class="img-insider" src="img/open.png" />
-          </div>
-          <img class="img-book" src="img/book.jpg" />
-        </div>
-        <!-- book example ----------- -->
+
+    <div id="search-message">SEARCH RESULTS</div>
+         <div id="books-container">
+              <!-- book example ----------- -->
+        <?php
+        $data = mysql_escape_string($_POST['search']);
+        $search = "select * from books where category like '%".$data."%' or  book_name  like '%".$data."%'";
+        $result = mysql_query($search);
+
+        if ($result) {
+          while ($row = mysql_fetch_array($result, MYSQL_BOTH)) {
+          //$bookName = $row ["book_image"];
+          //$autor = $row ["author"];
+          //$id = $row["book_id"];
+          //$image = $row["book_image"];
+          //$category = $row ["category"];
+
+
+
+          echo "<img class='img-book' src='http://localhost:8888/", $row["book_image"], "'/>";
+
+
+          //echo "book name:".$bookName." autor:".$autor." id ".$id." image:".$image." categoria:".$category."<br>";
+          //printf ("Name: %s  Image: %s", $row[1], $row[4] ,'<br />');
+          }
+        } else {
+          echo "nenhum resultado encontrado ";
+        }
+        ?>
+
       </div>
   </div>
-
-<?php
-$data = mysql_escape_string($_POST['search']);
-
-$search = "select * from books where category like '%".$data."%' or  book_name  like '%".$data."%'";
-
-$result = mysql_query($search);
-$num = mysql_num_rows($result);
-if ($result) {
-	while ($row = mysql_fetch_array($result, MYSQL_BOTH)) {
-	$bookName = $row ["book_image"];
-	$autor = $row ["author"];
-	$id = $row["book_id"];
-	$image = $row["book_image"];
-	$category = $row ["category"];
-
-
-	echo "book name:".$bookName." autor:".$autor." id ".$id." image:".$image." categoria:".$category."<br>";
-	//printf ("Name: %s  Image: %s", $row[1], $row[4] ,'<br />');
-	}
-} else {
-	echo "nenhum resultado encontrado ";
-}
-?>
 
 
 <?php
