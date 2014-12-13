cb7c10790db20148739971818a40646919be79d6
Jessica Furtado <jessica@student.dei.uc.pt>
2014-12-13 13:55:38 +0000

    up

--- read.php
+++ read.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<!--<?php include ('includes/connect.php');?>-->
+<?php include ('includes/connect.php');?>
 <html lang="en">
 <head>
   <meta charset="utf-8">
@@ -18,6 +18,11 @@
   <link href="css/font-awesome-min.css" rel="stylesheet" type="text/css">
   <link href="css/read.css" rel="stylesheet" type="text/css">
 
+  <!-- Bibliotecas -->
+  <script src="functions/XMLWriter-1.0.0.js"></script>
+  <!-- folhas de JS -->
+  <script src="functions/functions.js"></script>
+
   <!-- IE8 support for HTML5 elements and media queries -->
   <!--[if lt IE 9]>
   <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
@@ -28,8 +33,8 @@
 <body>
 
   <nav id="read-nav">
-    <img class="read-nav-a" href="preview.php" width="30px" src="http://iconmonstr.com/g/gd/makefg.php?i=s2/default/iconmonstr-book-5-icon.png&r=255&g=255&b=255"/><br><br><br>
-    <img class="read-nav-a" href="#" width="30px" src="http://iconmonstr.com/g/gd/makefg.php?i=s2/default/iconmonstr-quote-13-icon.png&r=255&g=255&b=255"/><br><br><br>
+    <img class="read-nav-a" width="30px" src="http://iconmonstr.com/g/gd/makefg.php?i=s2/default/iconmonstr-book-5-icon.png&r=255&g=255&b=255"/><br><br><br>
+    <img class="read-nav-a"  width="30px" src="http://iconmonstr.com/g/gd/makefg.php?i=s2/default/iconmonstr-quote-13-icon.png&r=255&g=255&b=255"/><br><br><br>
     <img class="read-nav-a" id="starRating" href="#" width="30px" src="http://iconmonstr.com/g/gd/makefg.php?i=s2/default/iconmonstr-star-2-icon.png&r=255&g=255&b=255"/><br><br><br>
     <img class="read-nav-a" id="editText" href="#" width="30px" src="http://iconmonstr.com/g/gd/makefg.php?i=s2/default/iconmonstr-font-size-icon.png&r=255&g=255&b=255"/><br><br><br>
     <img class="read-nav-a" href="#" width="30px" src="http://iconmonstr.com/g/gd/makefg.php?i=s2/default/iconmonstr-plus-icon.png&r=255&g=255&b=255"/><br><br><br>
@@ -54,30 +59,21 @@
   </div>
 
       <!-- Texto do Livro -->
-      <div id="read-text">
-        <p>Mr. And Mrs. Dursley, of number four, <a class="word-link">Privet Drive</a>, were proud to say that they were perfectly normal, thank you very much. They were the last people you'd expect to be involved in anything strange ormysterious, because they just didn't hold with such nonsense. </p> <p> Mr. Dursley was the director of a firm called Grunnings, which made drills. He was a big, beefy man withhardly any neck, although he did have a very large mustache. Mrs. Dursley was thin and blonde and hadnearly twice the usual amount of neck, which came in very useful as she spent so much of her time craningover garden fences, spying on the neighbors. The Dursleys had a small son called Dudley and in theiropinion there was no finer boy anywhere. </p><p>The Dursleys had everything they wanted, but they also had a secret, and their greatest fear was thatsomebody would discover it. They didn't think they could bear it if anyone found out about the Potters. Mrs. Potter was Mrs. Dursley's sister, but they hadn't met for several years; in fact, Mrs. Dursley pretendedshe didn't have a sister, because her sister and her good-for-nothing husband were as unDursleyish as itwas possible to be. The Dursleys shuddered to think what the neighbors would say if the Potters arrived inthe street. The Dursleys knew that the Potters had a small son, too, but they had never even seen him. Thisboy was another good reason for keeping the Potters away; they didn't want Dudley mixing with a child like that.</p><p> When Mr. And Mrs. Dursley woke up on the dull, gray Tuesday our story starts, there was nothing aboutthe cloudy sky outside to suggest that strange and mysterious things would soon be happening all over thecountry. Mr. Dursley hummed as he picked out his most boring tie for work, and Mrs. Dursley gossipedaway happily as she wrestled a screaming Dudley into his high chair. </p><p>None of them noticed a large, tawny owl flutter past the window. </p><p>At half past eight, Mr. Dursley picked up his briefcase, pecked Mrs. Dursley on the cheek, and tried to kissDudley good-bye but missed, because Dudley was now having a tantrum and throwing his cereal at thewalls. "Little tyke, " chortled Mr. Dursley as he left the house. He got into his car and backed out of numberfour's drive. It was on the corner of the street that he noticed the first sign of something peculiar -- a cat reading amap. For a second, Mr. Dursley didn't realize what he had seen -- then he jerked his head around to look again. There was a tabby cat standing on the corner of Privet Drive, but there wasn't a map in sight. What could he have been thinking of? It must have been a trick of the light. Mr. Dursley blinked and stared at the cat. It stared back. As Mr. Dursley drove around the corner and up the road, he watched the cat in his mirror. It was now reading the sign that said Privet Drive -- no, looking at the sign; cats couldn't read maps or signs. Mr. Dursley gave himself a little shake and put the cat out of his mind. As he drove toward townhe thought of nothing except a large order of drills he was hoping to get that day. </p><p>But on the edge of town, drills were driven out of his mind by something else. As he sat in the usualmorning traffic jam, he couldn't help noticing that there seemed to be a lot of strangely dressed peopleabout. People in cloaks. Mr. Dursley couldn't bear people who dressed in funny clothes -- the getups you saw on young people!</p>
-        <!--<div id="read-text-video">
-          <iframe width="420" height="315"
-          src="http://www.youtube.com/embed/8GFm9NMFJUg">
-        </iframe>
-      </div>
+      <div id="read-text" class="editavel">
+        <p contenteditable="true">Mr. And Mrs. Dursley, of number four, <a class="word-link">Privet Drive</a>, were proud to say that they were perfectly normal, thank you very much. They were the last people you'd expect to be involved in anything strange ormysterious, because they just didn't hold with such nonsense. </p> <p> Mr. Dursley was the director of a firm called Grunnings, which made drills. He was a big, beefy man withhardly any neck, although he did have a very large mustache. Mrs. Dursley was thin and blonde and hadnearly twice the usual amount of neck, which came in very useful as she spent so much of her time craningover garden fences, spying on the neighbors. The Dursleys had a small son called Dudley and in theiropinion there was no finer boy anywhere. </p><p>The Dursleys had everything they wanted, but they also had a secret, and their greatest fear was thatsomebody would discover it. They didn't think they could bear it if anyone found out about the Potters. Mrs. Potter was Mrs. Dursley's sister, but they hadn't met for several years; in fact, Mrs. Dursley pretendedshe didn't have a sister, because her sister and her good-for-nothing husband were as unDursleyish as itwas possible to be. The Dursleys shuddered to think what the neighbors would say if the Potters arrived inthe street. The Dursleys knew that the Potters had a small son, too, but they had never even seen him. Thisboy was another good reason for keeping the Potters away; they didn't want Dudley mixing with a child like that.</p><p> When Mr. And Mrs. Dursley woke up on the dull, gray Tuesday our story starts, there was nothing aboutthe cloudy sky outside to suggest that strange and mysterious things would soon be happening all over thecountry. Mr. Dursley hummed as he picked out his most boring tie for work, and Mrs. Dursley gossipedaway happily as she wrestled a screaming Dudley into his high chair. </p><p>None of them noticed a large, tawny owl flutter past the window. </p><p>At half past eight, Mr. Dursley picked up his briefcase, pecked Mrs. Dursley on the cheek, and tried to kissDudley good-bye but missed, because Dudley was now having a tantrum and throwing his cereal at thewalls. "Little tyke, " chortled Mr. Dursley as he left the house. He got into his car and backed out of numberfour's drive. It was on the corner of the street that he noticed the first sign of something peculiar -- a cat reading amap. For a second, Mr. Dursley didn't realize what he had seen -- then he jerked his head around to look again. There was a tabby cat standing on the corner of Privet Drive, but there wasn't a map in sight. What could he have been thinking of? It must have been a trick of the light. Mr. Dursley blinked and stared at the cat. It stared back. As Mr. Dursley drove around the corner and up the road, he watched the cat in his mirror. It was now reading the sign that said Privet Drive -- no, looking at the sign; cats couldn't read maps or signs. Mr. Dursley gave himself a little shake and put the cat out of his mind. As he drove toward townhe thought of nothing except a large order of drills he was hoping to get that day. </p><p>But on the edge of town, drills were driven out of his mind by something else. As he sat in the usualmorning traffic jam, he couldn't help noticing that there seemed to be a lot of strangely dressed peopleabout. People in cloaks. Mr. Dursley couldn't bear people who dressed in funny clothes -- the getups you saw on young people!</p>
 
-
-      <div id="read-text-image">
-        <img src="img/porco.png">
-      </div>-->
     </div>
 
   <!--  <div id="commentsWritten">
       <p> </p>
-    </div>
+    </div>-->
 
     <div id="addComment">
       <p>Escrever Comentário</p></br>
       <input type="text" style="width:98%; height:160px;" id="commentArea"><br><br>
       <input type="submit" id="commentBtn" value="Comentar">
     </div>
-
+<!--
     <div id="addImage">
       <form action="">
         <p>Adicionar Imagem</p></br>
@@ -146,6 +142,32 @@
 
     $(helpIcon).click(function(){
         $(popInst).addClass("open");
+    });
+
+    $( "#read-text" ).click(divClicked);
+    //função para o click e não click
+    function divClicked() {
+      var editableText = $("<textarea />");
+      editableText.val($(this).html());
+      $(this).replaceWith(editableText);
+      editableText.focus();
+      editableText.blur(editableTextBlurred);
+    }
+
+    function editableTextBlurred() {
+      // Preserve the value of textarea
+      var html = $(this).val();
+      // create a dynamic div
+      var viewableText = $("<div id='read-text'> ");
+      // set it's html
+      viewableText.html(html);
+      // replace out the textarea
+      $(this).replaceWith(viewableText);
+    }
+
+    $('#commentBtn').click(function(){
+      $('#addComment').removeClass("open");
+      $('#commentsWritten p').append("<p>"+$("#commentArea").val()+"<p>"+"<p>@sergiomrebelo</p>");
     });
 
     /*var fontSize = parseInt($("body").css("font-size"));
@@ -262,10 +284,7 @@
         }
       });
 
-      $('#commentBtn').click(function(){
-        $('#addComment').removeClass("open");
-        $('#commentsWritten p').append("<p>"+$("#commentArea").val()+"<p>"+"<p>@sergiomrebelo</p>");
-      });
+
 
       $("#imageBtn").click(function(){
         $('#addImage').removeClass("open");
