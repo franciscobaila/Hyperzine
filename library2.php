8d245b7a73391b8c878e261df990764f3676dc34
xufuou <xufuou@gmail.com>
2014-12-14 01:31:04 +0000

    new

--- library2.php
+++ library2.php
@@ -18,7 +18,7 @@ include ('includes/header.php');
          //$image = $row["book_image"];
          //$category = $row ["category"];
 
-         echo "<div id='book-1' class='book",$row["category"],"read'>";
+         echo "<div id='book-1' class='book ",$row["category"],"'>";
 
          echo "<img class='img-book' src='http://localhost:8888/", $row["book_image"], "'/>";
          echo "</div>";
@@ -29,35 +29,37 @@ include ('includes/header.php');
        } else {
          echo "Biblioteca vazia";
        }
+       
        ?>
 
-      <div id="filter">
-        <!-- filtro ordenar ----------- -->
-        <div id="order-by" class="filters">
-          <div class="filter-title">ORDER BY</div>
-          <form action="">
-            <p><input id="check1" type="checkbox" name="categoria" value="check1"><label for="check1">Name</label></p>
-            <p><input id="check2" type="checkbox" name="categoria" value="check1"><label for="check2">Creation Date</label></p>
-            <p><input id="check3" type="checkbox" name="categoria" value="check2"><label for="check3">Classification</label></p>
-          </form>
-        </div>
-        <!-- filtro ordenar ----------- -->
-        <!-- filtro categorias ----------- -->
-        <div id="categories" class="filters filters2">
-          <div class="filter-title">CATEGORIES</div>
-          <ul id="filters">
-            <li>
-              <p><input id="check2.1" type="checkbox" name="categoria" value="fiction" checked><label for="check2.1">Fiction</label></p>
-              <p><input id="check2.2" type="checkbox" name="categoria" value="love" checked><label for="check2.2">Love Story</label></p>
-              <p><input id="check2.3" type="checkbox" name="categoria" value="drama" checked><label for="check2.3">Drama</label></p>
-              <p><input id="check2.4" type="checkbox" name="categoria" value="fan" checked><label for="check2.4">Fan Fiction</label></p>
-              <p><input id="check2.5" type="checkbox" name="categoria" value="cenas" checked><label for="check2.5">Others</label></p>
-            </li>
-          </ul>
-        </div>
-        <!-- filtro categorias ----------- -->
-      </div>
-  </div>
+            <div id="filter">
+              <!-- filtro ordenar ----------- -->
+              <div id="order-by" class="filters">
+                <div class="filter-title">ORDER BY</div>
+                <form action="">
+                  <p><input id="check1" type="checkbox" name="categoria" value="check1"><label for="check1">Name</label></p>
+                  <p><input id="check2" type="checkbox" name="categoria" value="check1"><label for="check2">Creation Date</label></p>
+                  <p><input id="check3" type="checkbox" name="categoria" value="check2"><label for="check3">Classification</label></p>
+                </form>
+              </div>
+              <!-- filtro ordenar ----------- -->
+              <!-- filtro categorias ----------- -->
+              <div id="categories" class="filters filters2">
+                <div class="filter-title">CATEGORIES</div>
+                <ul id="filters">
+                  <li>
+                    <p><input id="check2.1" type="checkbox" name="categoria" value="fiction" checked><label for="check2.1">Fiction</label></p>
+                    <p><input id="check2.2" type="checkbox" name="categoria" value="love" checked><label for="check2.2">Love Story</label></p>
+                    <p><input id="check2.3" type="checkbox" name="categoria" value="drama" checked><label for="check2.3">Drama</label></p>
+                    <p><input id="check2.4" type="checkbox" name="categoria" value="fan" checked><label for="check2.4">Fan Fiction</label></p>
+                    <p><input id="check2.5" type="checkbox" name="categoria" value="cenas" checked><label for="check2.5">Others</label></p>
+
+                  </li>
+                </ul>
+              </div>
+    <!-- filtro categorias ----------- -->
+    </div>
+</div>
 
 <?php
 include ('includes/footer.php');
